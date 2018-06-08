45a4fb86fb68b82a0b5ad18e386f71990b747a63
Andrew Le <thanhan287@gmail.com>
2018-06-08 11:17:30 -0400

    Add files via upload

---
+++ run_winning_predictor.py
@@ -0,0 +1,19 @@
+# import pandas, NEAT, etc
+import neat
+
+# get data + input for neural network
+
+# classes
+
+# functions to train, etc
+
+# store best fit algorithm
+
+def run_most_fit_result():
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, 'config')
+
+def main():
+    if :
+    
+    else:
+        run_most_fit_result()
---
+++ train.py
@@ -0,0 +1,3 @@
+import neat
+
+def main():
