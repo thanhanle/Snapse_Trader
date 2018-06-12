c3194f68a7b40f96b265f2de406f3da3d7b54e41
tal <thanhan287@gmail.com>
2018-06-12 16:57:15 -0400

    files for getting data from robinhood

---
+++ robinhood_price_grab.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import datetime as dt
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+#style.use('ggplot')
+
+start_time = dt.datetime(2017,1,12)
+end_time = dt.datetime.now()
+
+data_frame = web.DataReader('TSLA', 'robinhood', start_time, end_time)
+closing_prices = data_frame['close_price'].values
+
+print(closing_prices)
+data_frame.to_csv('price3.csv')  
---
+++ robinhood_train_window.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+prices = pd.read_csv('price3.csv')
+
+class sliding_trainer():
+    def __init__(self, prices, stepsize, lookback):
+        self.prices = prices
+        self.stepsize = stepsize
+        self.lookback = lookback
+        self.position = self.lookback
+        self.inputs = self.prices[self.position - self.lookback:self.position]
+        self.testdata = self.prices[self.position-1:self.position + self.stepsize]
+
+    def slidestep(self):
+        if (self.position + self.stepsize) <= len(self.prices):
+            self.position += self.stepsize
+        else:
+            self.position = self.lookback
+            return "done","done"
+        self.inputs = self.prices[self.position-self.lookback:self.position]
+        self.testdata = self.prices[self.position-1:self.position + self.stepsize]
+        return self.inputs.values, self.testdata.values
